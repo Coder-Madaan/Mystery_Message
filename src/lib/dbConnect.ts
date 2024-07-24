826ad93aec2bc8c9af6d90709643c9539db6341a
Vaibhav <madaanvaibhav001@gmail.com>
2024-07-24 15:00:56 +0530

    MongoDB connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+type connectionObject={
+    isConnected?:number;
+}
+
+const connection:connectionObject = {};
+
+async function dbConnect():Promise<void>{
+    if(connection.isConnected){
+        console.log('Already connected to database');
+        return;
+    }
+    try{
+       const db= await mongoose.connect(process.env.MONGODB_URI || '',{})
+        connection.isConnected = db.connections[0].readyState;
+        console.log('Connected to database');
+    }catch(err){
+        console.log('database connection failed',err);
+        process.exit(1);
+    }
+}
